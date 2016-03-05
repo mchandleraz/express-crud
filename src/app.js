aa1df6ae506f67172123e11186b023e2c74450ad
Matt Chandler <mcaz689@gmail.com>
2016-03-04 21:36:56 -0700

    add mongoose

--- src/app.js
+++ src/app.js
@@ -13,7 +13,6 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
-// uncomment after placing your favicon in /public
 // app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
@@ -49,7 +48,6 @@ if (app.get('env') === 'development') {
     });
   });
 }
-
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
